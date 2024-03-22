98f94dd6f3183c631431d2b3b7e5f2669461bfa4
edanurkorkmaz <korkmaze101@gmail.com>
2024-03-22 15:54:56 +0300

    çalışmalar yüklendi.

---
+++ kosulIfElse.py
@@ -0,0 +1,27 @@
+username = "edanur"
+parola = 'eda123'
+
+if(username == "edanur"):
+ if(parola == "eda123"):
+    print("hoşgeldiniz")
+ else:
+     print("şifreniz yanlış")
+    
+else:
+    print("username yanlış")     
+      
+#-------------------------------------
+    
+    
+x = int(input("x :"))
+y = int(input("y :"))
+
+if x>y:
+    print("x y den büyük")
+    
+elif x == y:
+    print("x ve y eşittir.")
+    
+else:
+    print("y x den büyük")
+
