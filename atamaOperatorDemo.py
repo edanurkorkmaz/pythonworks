e126bfe3baa3becc6c8c24a55bd44ff8b762aa6b
edanurkorkmaz <korkmaze101@gmail.com>
2024-03-22 15:31:14 +0300

    çalışmalar yüklendi.

---
+++ atamaOperatorDemo.py
@@ -0,0 +1,43 @@
+x, y, z = 2, 5, 10
+
+numbers = 1, 5, 7, 10, 6
+
+#kullanıcıdan alacağınız 2 sayının çarpımı ile x,y,z toplamının farkı nedir ?
+#toplam = x + y + z
+
+#a = int(input("ilk sayıyı girin: "))
+#b = int(input("2.sayıyı girin: "))
+
+#carpım = (a * b)
+
+#print(carpım - toplam) 
+
+# y'nin x'e kalansız bölümünü hesaplayın.
+
+#y //= x
+#print(y)
+
+#(x,y,z) toplamnının mod 3'ü nedir?
+
+#toplam = (x + y + z)
+#result = toplam % 3
+#print(result)
+
+#y'nin x. kuvvetini hesaplayınız.
+#y**= x
+#print(y)
+
+#result = y ** x
+#print(result)
+
+#x, *y, z = numbers işlemine göre z'nin küpü kaçtır?
+
+#x, *y, z = 1, 5, 7, 10, 6
+#result = z ** 3
+#print(result)
+
+#x, *y, z = numbers işlemine göre y'nin değerleri toplamı nedir?
+x, *y, z = 1, 5, 7, 10, 6
+result = y[0] + y[1] + y[2]
+
+print(result)
---
+++ atamaOperatorleri.py
@@ -0,0 +1,17 @@
+#values = 1, 2
+
+#print(values)
+#print(type(values))
+
+
+#x, y, z = values
+
+
+#print(x, y, z)
+
+
+values = 1, 2, 3, 4, 5
+
+x, y, *z = values
+
+print(x, y, z)
