6d592062c697c0bcd7552a2f38d58529f1420f57
edanurkorkmaz <korkmaze101@gmail.com>
2024-03-26 15:27:18 +0300

    bugünün çalışmaları eklendi.

---
+++ sayıDeğerDegistirme.py
@@ -0,0 +1,12 @@
+
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
+
+
+print("Birinci sayı:", sayi1)
+print("İkinci sayı:", sayi2)
+
+
+print("Değiştirildikten sonra:")
+print("Birinci sayı:", sayi2)
+print("İkinci sayı:", sayi1)
