d3572dcdde35a98c92c06bc4c4d8b5faa47e587c
edanurkorkmaz <korkmaze101@gmail.com>
2024-03-26 16:18:08 +0300

    task 1 ödevleri yapıldı.

---
+++ aynıHarf.py
@@ -0,0 +1,7 @@
+metin = "Afyonkarahisarlılaştıramadıklarımızdanmısınız"
+
+aranan_harf = input("Lütfen bir harf girin: ")
+
+frekans = metin.count(aranan_harf)
+
+print(f"'{aranan_harf}' harfi metin içinde {frekans} kez geçiyor.")
---
+++ hipotenus.py
@@ -0,0 +1,8 @@
+import math
+
+a = float(input("Birinci dik kenar uzunluğunu girin: "))
+b = float(input("İkinci dik kenar uzunluğunu girin: "))
+
+c = math.sqrt(a**2 + b**2)
+
+print("Hipotenüs uzunluğu:", c)
