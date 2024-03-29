cb69b360a2f9ef767eaf48d59950a09c4d5d4140
edanurkorkmaz <korkmaze101@gmail.com>
2024-03-29 09:54:19 +0300

    modüller kodları eklendi.

---
+++ modul.py
@@ -0,0 +1,14 @@
+# import math
+# import math as islem
+# #value = dir(math)
+# #value = help(math)
+# #value = help(math.factorial)
+# value = math.sqrt(49)
+# value = math.factorial(5)
+# value = math.(5.9)
+
+# value = islem.factorial(5)
+from math import*
+value = factorial(5)
+
+print(value)
