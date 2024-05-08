2ea0a92802d3608fd8a7fcb324ed63071e7752e8
edanurkorkmaz <korkmaze101@gmail.com>
2024-05-08 15:52:05 +0300

    modül konusu çalışıldı.

---
+++ moduller.py
@@ -0,0 +1,8 @@
+import modulum
+
+print(modulum.topla(20,35))
+print(modulum.CevreHesapla(2))
+
+
+from modulum import *
+
---
+++ modulum.py
@@ -0,0 +1,10 @@
+from math import pi
+
+def topla(a,b):
+    return a + b
+
+def CevreHesapla(r):
+    return 2 * pi * r
+
+if __name__ == '__main__':
+ print("eda")
